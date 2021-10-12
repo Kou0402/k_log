96015145de0c0c2a78f1ad92441f1d3c91e763b2
kou0402 <kou.4869.biz@gmail.com>
2021-10-12 17:33:45 +0900

    feat: ヘッダーコンポーネントのレイアウトを整形する

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+import { Box, Heading, Text } from '@chakra-ui/layout'
+
 export const Header = () => {
   return (
-    <header>
-      <h1>タイトル</h1>
-    </header>
+    <Box as="header" p="2">
+      <Heading>
+        <Link href="/">
+          <a>kou blog</a>
+        </Link>
+      </Heading>
+    </Box>
   )
 }
