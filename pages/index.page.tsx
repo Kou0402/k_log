cf613a47885f86cfafc9f0a2d367660763e4bb1b
kou0402 <kou.4869.biz@gmail.com>
2021-10-12 15:51:46 +0900

    chore: ダミーヘッダーを表示する

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
+import { Header } from "./Header";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Header />
       <main>ホームページ</main>
     </div>
   );
