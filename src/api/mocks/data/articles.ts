16926a4515b1d0104fff8fa4a7dd62904711e3b5
kou0402 <kou.4869.biz@gmail.com>
2021-10-31 23:28:55 +0900

    chore: mock を修正する

--- src/api/mocks/data/articles.ts
+++ src/api/mocks/data/articles.ts
@@ -5,13 +5,15 @@ export const getArticlesMock = (): FindAllArticleResponse => {
     contents: [
       {
         id: 'n--xjw8vp_1',
-        createdAt: new Date('2021-10-14'),
+        createdAt: '2021-10-14',
         title: 'ブログを作りました',
+        tags: [{ name: '日常' }],
       },
       {
         id: 'n--xjw8vp_2',
-        createdAt: new Date('2021-10-14'),
+        createdAt: '2021-10-14',
         title: 'テスト',
+        tags: [{ name: 'React' }],
       },
     ],
   }
